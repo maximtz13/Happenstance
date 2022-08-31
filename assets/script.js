7f32c3836d65f77a286643b207fb9fde5048f251
ascosper@hotmail.com <Amaya@Normas-Air.attlocal.net>
2022-08-30 22:15:17 -0500

    localStorage

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,5 @@
-// console.log("hi")
+var bandNameEl = document.getElementsByClassName("band".value)
+localStorage.setItem("search", bandNameEl)
+console.log(bandNameEl)
+window.localStorage.getItem("search")
 
-// localStorage.setItem(1,2)
-// localStorage.setItem("name","Amaya")
-// localStorage.getItem("name")
-// console.log(localStorage.getItem("name"))
-// console.log(localStorage.key(0))
-localStorage.setItem("input", document.querySelector("input").value)
-console.log(localStorage.getItem("input"))
